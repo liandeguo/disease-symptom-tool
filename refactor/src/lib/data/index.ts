f83519b32a293377a8ea19dfd51be64af239b731
liande <liande@guocn.org>
2026-05-22 16:59:05 +0200

    feat: add clinical trials

--- refactor/src/lib/data/index.ts
+++ refactor/src/lib/data/index.ts
@@ -4,7 +4,7 @@ import symptoms from './index.json' with { type: 'json'};
 // import Fuse from 'fuse.js';
 
 async function wikipediaAPI(query: any) {
-    const response = await fetch('https://en.wikipedia.org/api/rest_v1/page/summary/heart attack')
+    const response = await fetch('https://en.wikipedia.org/api/rest_v1/page/summary/' + query)
 
     if(!response.ok) {
         throw new Error('wikipedia is retardet')
@@ -14,13 +14,25 @@ async function wikipediaAPI(query: any) {
     return data.extract
 };
 
+async function clinicalTrialsAPI(query: any){
+    const response = await fetch('https://clinicaltrials.gov/api/v2/studies?format=json&query.cond=' + query +'&filter.overallStatus=ACTIVE_NOT_RECRUITING&sort=%40relevance&pageSize=3')
+     if (!response.ok) {
+        throw new Error('clinicaltrials org is dumb')
+     }
+
+     const data = await response.json()
+     return data.studies
+}
+
 export async function resolveCondition(slug: Number) {
     const condition = conditions.find((c: any) => c.id === slug)
     const wikipediaResults = await wikipediaAPI(condition?.name)
-    
+    const clinicalTrials = await clinicalTrialsAPI(condition?.name)
+
     const data = {
         ...condition,
-        summary: await wikipediaResults
+        summary: await wikipediaResults,
+        clinicalTrials: await clinicalTrials
     }
     return data
     // return conditions.find((c: any) => 
--- refactor/src/routes/conditions/[slug]/+page.svelte
+++ refactor/src/routes/conditions/[slug]/+page.svelte
@@ -27,6 +27,8 @@
     const symptomsName = data.condition.symptoms.map(item => item.symptom)
     const symptomsPercentage = data.condition.symptoms.map(item => item.percentage)
 
+    const clinicalTrials = data.condition.clinicalTrials
+
 </script>
 <main class="flex">
     <!-- Main -->
@@ -58,33 +60,57 @@
             </Plot>
             
         </div>
+
         <hr class="my-6">
-    <div class="treatmentDiagnosis flex gap-8">
-        <!-- Procedures -->
-        <div class="procedures flex-1">
-            <h3 class="uppercase font-medium mb-2">Common tests & procedures</h3>
-            <div class="flex flex-wrap gap-1">
-                {#each procedures as procedure}
-                    <span class="badge">
-                        {procedure}
-                    </span>
-                {/each}
+
+        <div class="treatmentDiagnosis flex gap-8">
+            <!-- Procedures -->
+            <div class="procedures flex-1">
+                <h3 class="uppercase font-medium mb-2">Common tests & procedures</h3>
+                <div class="flex flex-wrap gap-1">
+                    {#each procedures as procedure}
+                        <span class="badge">
+                            {procedure}
+                        </span>
+                    {/each}
+                </div>
+            </div>
+
+            <!-- Treatments -->
+            <div class="treatments flex-1">
+                <h3 class="uppercase font-medium mb-2">Common treatments</h3>
+                <div class="flex flex-wrap gap-1">
+                    {#each treatments as treatment}
+                        <span class="badge">
+                            {treatment}
+                        </span>
+                    {/each}
+                </div>
             </div>
         </div>
 
-        <!-- Treatments -->
-        <div class="treatments flex-1">
-            <h3 class="uppercase font-medium mb-2">Common treatments</h3>
-            <div class="flex flex-wrap gap-1">
-                {#each treatments as treatment}
-                    <span class="badge">
-                        {treatment}
-                    </span>
-                {/each}
-            </div>
-        </div>
-    </div>
+        <hr class="my-6">
+        <div class="clinicalTrials">
+            <h3 class="uppercase font-medium mb-2">Clinical Trials</h3>
+            {#each clinicalTrials as trial}
+                <a href="https://clinicaltrials.gov/study/{trial.protocolSection.identificationModule.nctId}" target="_blank">
+                    <div class="clinicalTrial mb-3 relative">
+                        <span class="font-semibold">
+                            {trial.protocolSection.identificationModule.officialTitle}
+                        </span> <br>
+                        {trial.protocolSection.identificationModule.organization.fullName}
+                        <br>
+                        <div class="flex flex-wrap gap-1">
+                            <span class="badge">{trial.protocolSection.statusModule.startDateStruct.date} - {trial.protocolSection.statusModule.primaryCompletionDateStruct.date}</span>           
+                            <span class="badge">{trial.protocolSection.designModule.enrollmentInfo.count} participants</span>
+                        </div>
 
+                    </div>
+                </a>
+            {/each}
+        </div>
+
+        
     </section>  
     <!-- Context -->
     <!-- <div class="contextMenu bg-white w-[340px] px-4 pt-8">
