e006e54ded2a000625fab92d9aa46df22f41e866
liande <liande@guocn.org>
2026-05-31 13:06:23 +0200

    chore: put new version into base folder

--- app/src/lib/components/ui/alert-dialog/alert-dialog.svelte
+++
@@ -1,7 +0,0 @@
-<script>
-	import { AlertDialog as AlertDialogPrimitive } from "bits-ui";
-
-	let { open = $bindable(false), ...restProps } = $props();
-</script>
-
-<AlertDialogPrimitive.Root bind:open {...restProps} />
